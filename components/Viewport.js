55f1665c6f8de8a5d93ef3bd1b826637a3bf1c2a
yhc9308 <yhc9308@naver.com>
2023-09-25 12:52:23 +0900

    알렉산더님 코너스톤

--- components/Viewport.js
+++ components/Viewport.js
@@ -1,3 +1,4 @@
+// 필요한 모듈과 라이브러리를 임포트
 import {
     RenderingEngine,
     Enums,
@@ -18,164 +19,180 @@ import { toolNames } from '../utils/cornerstone3D/initCornerstoneTools'
 import  DICOMSRDisplayTool from '../utils/cornerstone-dicom-sr/src/tools/DICOMSRDisplayTool';
 import managerInit from './managerInit';
 
-const { ViewportType} = Enums;
+// 필요한 Enum 값을 디스트럭처링
+const { ViewportType } = Enums;
+
+// 주요 실행 함수
 async function run(element) {
-    // Init Cornerstone and related libraries
-    await initCornerstone();
+  // Init Cornerstone and related libraries
+  // Cornerstone 및 관련 라이브러리 초기화
+  await initCornerstone();
 
-    // Get Cornerstone imageIds and fetch metadata into RAM
-    const imageIds = await createImageIdsAndCacheMetaData({
-      StudyInstanceUID: "1.400.20.81.610.0002598612943543",
-      SeriesInstanceUID: "1.3.46.670589.30.1.6.1.1625523293.1622603492578.3",
-      wadoRsRoot: "http://localhost/orthanc/dicom-web",
-    });
-   
-    const renderingEngineId = 'myRenderingEngine';
-    const viewportId = 'CT_AXIAL_STACK';
-    const renderingEngine = new RenderingEngine(renderingEngineId);
+  // Get Cornerstone imageIds and fetch metadata into RAM
+  // Cornerstone 이미지 ID 및 메타데이터 획득
+  const imageIds = await createImageIdsAndCacheMetaData({
+    StudyInstanceUID: "1.400.20.81.610.201712061281",
+    SeriesInstanceUID: "1.3.46.670589.30.1.6.1.1625523293.1512518914734.3",
+    wadoRsRoot: "http://localhost/orthanc/dicom-web",
+  });
 
-    const viewportInput = {
-        viewportId,
-        element,
-        type: ViewportType.STACK,
-    };
+  // 렌더링 엔진과 뷰포트 설정
+  const renderingEngineId = "myRenderingEngine";
+  const viewportId = "CT_AXIAL_STACK";
+  const renderingEngine = new RenderingEngine(renderingEngineId);
 
-    renderingEngine.enableElement(viewportInput);
-    const viewport = renderingEngine.getViewport(viewportInput.viewportId);
+  const viewportInput = {
+    viewportId,
+    element,
+    type: ViewportType.STACK,
+  };
 
-    const toolGroupId = 'CT_TOOLGROUP';
-    // Define tool groups to add the segmentation display tool to
-   
-    //addTool(DICOMSRDisplayTool) //! why in init rs not working ?
-    const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
-    
-    function  _getToolNames(toolGroupTools) {
-        const toolNames = [];
-        if (toolGroupTools.active) {
-            toolGroupTools.active.forEach(tool => {
-                toolNames.push(tool.toolName);
-            });
-        }
-        if (toolGroupTools.passive) {
-            toolGroupTools.passive.forEach(tool => {
-                toolNames.push(tool.toolName);
-            });
-        }
+  // 렌더링 엔진 초기화 및 뷰포트 획득
+  renderingEngine.enableElement(viewportInput);
+  const viewport = renderingEngine.getViewport(viewportInput.viewportId);
 
-        if (toolGroupTools.enabled) {
-            toolGroupTools.enabled.forEach(tool => {
-                toolNames.push(tool.toolName);
-            });
-        }
+  const toolGroupId = "CT_TOOLGROUP";
+  // Define tool groups to add the segmentation display tool to
 
-        if (toolGroupTools.disabled) {
-            toolGroupTools.disabled.forEach(tool => {
-                toolNames.push(tool.toolName);
-            });
-        }
+  // 도구 그룹 생성
+  //addTool(DICOMSRDisplayTool) //! why in init rs not working ?
+  const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
 
-        return toolNames;
+  // 도구 이름을 가져오는 함수
+  function _getToolNames(toolGroupTools) {
+    const toolNames = [];
+    if (toolGroupTools.active) {
+      toolGroupTools.active.forEach((tool) => {
+        toolNames.push(tool.toolName);
+      });
     }
-    const addTools = (toolGroup, configs) => {
-        const toolNames = _getToolNames(tools);
-        toolNames.forEach(toolName => {
-            const toolConfig = configs[toolName] ?? {}
-            toolGroup.addTool(toolName, { ...toolConfig })
-        })
+    if (toolGroupTools.passive) {
+      toolGroupTools.passive.forEach((tool) => {
+        toolNames.push(tool.toolName);
+      });
     }
-    const tools = {
-        active: [
-            
-            {
-                toolName: toolNames.Pan,
-                bindings: [{ mouseButton: csToolsEnums.MouseBindings.Auxiliary }],
-            },
-            {
-                toolName: toolNames.Zoom,
-                bindings: [{ mouseButton: csToolsEnums.MouseBindings.Secondary }],
-            },
-            { toolName: toolNames.StackScrollMouseWheel, bindings: [] },
-        ],
-        passive: [
-            { toolName: toolNames.Magnify },
-            { toolName: toolNames.PlanarFreehandROI },
-            { toolName: DICOMSRDisplayTool.toolName },
-        ],
-        // enabled
-        // disabled
-        //disabled: [{}],//toolName: toolNames.ReferenceLines 
-    };
-    function setToolsMode(toolGroup, tools) {
-        const { active, passive, enabled, disabled } = tools;
 
-        if (active) {
-            active.forEach(({ toolName, bindings }) => {
-                toolGroup.setToolActive(toolName, { bindings });
-            });
-        }
-
-        if (passive) {
-            passive.forEach(({ toolName }) => {
-                toolGroup.setToolPassive(toolName);
-            });
-        }
-
-        if (enabled) {
-            enabled.forEach(({ toolName }) => {
-                toolGroup.setToolEnabled(toolName);
-            });
-        }
-
-        if (disabled) {
-            disabled.forEach(({ toolName }) => {
-                toolGroup.setToolDisabled(toolName);
-            });
-        }
+    if (toolGroupTools.enabled) {
+      toolGroupTools.enabled.forEach((tool) => {
+        toolNames.push(tool.toolName);
+      });
     }
-    addTools(toolGroup, tools)
-    setToolsMode(toolGroup, tools)
- 
-    toolGroup.addViewport(viewportId, renderingEngineId);
 
-    // set interpolation agressiveness while adding new annotation (ps: this does not change if interpolation is ON or OFF)
-    toolGroup.setToolConfiguration(PlanarFreehandROITool.toolName, {
-        interpolation: { knotsRatioPercentageOnAdd: 30 },
+    if (toolGroupTools.disabled) {
+      toolGroupTools.disabled.forEach((tool) => {
+        toolNames.push(tool.toolName);
+      });
+    }
+
+    return toolNames;
+  }
+  // 도구 추가 함수
+  const addTools = (toolGroup, configs) => {
+    const toolNames = _getToolNames(tools);
+    toolNames.forEach((toolName) => {
+      const toolConfig = configs[toolName] ?? {};
+      toolGroup.addTool(toolName, { ...toolConfig });
     });
+  };
 
-    // set interpolation to be ON while editing only
-    toolGroup.setToolConfiguration(PlanarFreehandROITool.toolName, {
-        interpolation: { interpolateOnAdd: false, interpolateOnEdit: true },
-    });
-    viewport.setStack(imageIds, imageIds.length-1);
-    viewport.render();
+  // 사용할 도구 설정
+  const tools = {
+    active: [
+      {
+        toolName: toolNames.Pan,
+        bindings: [{ mouseButton: csToolsEnums.MouseBindings.Auxiliary }],
+      },
+      {
+        toolName: toolNames.Zoom,
+        bindings: [{ mouseButton: csToolsEnums.MouseBindings.Secondary }],
+      },
+      { toolName: toolNames.StackScrollMouseWheel, bindings: [] },
+    ],
+    passive: [
+      { toolName: toolNames.Magnify },
+      { toolName: toolNames.PlanarFreehandROI },
+      { toolName: DICOMSRDisplayTool.toolName },
+    ],
+  };
 
+  // 도구의 모드 설정 함수
+  function setToolsMode(toolGroup, tools) {
+    const { active, passive, enabled, disabled } = tools;
 
+    // 각 상태별로 도구 설정
+    if (active) {
+      active.forEach(({ toolName, bindings }) => {
+        toolGroup.setToolActive(toolName, { bindings });
+      });
+    }
 
+    if (passive) {
+      passive.forEach(({ toolName }) => {
+        toolGroup.setToolPassive(toolName);
+      });
+    }
+
+    if (enabled) {
+      enabled.forEach(({ toolName }) => {
+        toolGroup.setToolEnabled(toolName);
+      });
+    }
+
+    if (disabled) {
+      disabled.forEach(({ toolName }) => {
+        toolGroup.setToolDisabled(toolName);
+      });
+    }
+  }
+  // 도구와 도구 모드 설정
+  addTools(toolGroup, tools);
+  setToolsMode(toolGroup, tools);
+
+  // 뷰포트 추가
+  toolGroup.addViewport(viewportId, renderingEngineId);
+
+  // set interpolation agressiveness while adding new annotation (ps: this does not change if interpolation is ON or OFF)
+  // 주석 도구 설정
+  toolGroup.setToolConfiguration(PlanarFreehandROITool.toolName, {
+    interpolation: { knotsRatioPercentageOnAdd: 30 },
+  });
+
+  // set interpolation to be ON while editing only
+  toolGroup.setToolConfiguration(PlanarFreehandROITool.toolName, {
+    interpolation: { interpolateOnAdd: false, interpolateOnEdit: true },
+  });
+  viewport.setStack(imageIds, imageIds.length - 1);
+  viewport.render();
 }
 
+// 뷰포트 컴포넌트
 export default function Viewport() {
-    const viewer = useRef(null);
-    const [managers, setManagers] = useState(null);
-    const defaultConfig = {
-      extensions: [],
-    };
+  // 참조와 상태 설정
+  const viewer = useRef(null);
+  const [managers, setManagers] = useState(null);
+  const defaultConfig = {
+    extensions: [],
+  };
 
-    useEffect(() => {
-        if (window !== undefined) {
-            run(viewer.current);
-            const runManagerInit = async () => {
-              managerInit(defaultConfig, [], [])
-                .then(setManagers)
-                .catch(console.error);
-            };
+  useEffect(() => {
+      if (window !== undefined) {
+        // 초기화 및 매니저 설정
+        run(viewer.current);
+        const runManagerInit = async () => {
+          managerInit(defaultConfig, [], [])
+            .then(setManagers)
+            .catch(console.error);
+        };
 
-            runManagerInit();
-        }
+        runManagerInit();
+      }
+  }, []);
 
-    }, [])
-    
-
-    
-    return (<div id='cornerstone-element' ref={viewer} style={{ width: 1024, height: 1024 }}></div>
-    )
+  return (
+    <div
+      id="cornerstone-element"
+      ref={viewer}
+      style={{ width: 1024, height: 1024 }}
+    ></div>
+  );
 }
